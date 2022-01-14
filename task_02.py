a7daa7aaab5d6f7e4a66d5112849db0833aaebc6
Denis <deniska.bayandin@yandex.ru>
2022-01-14 15:19:37 +0300

    edited_task_02

--- task_02.py
+++ task_02.py
@@ -1,19 +1,17 @@
-def coincidence(list, range):
-    if list=='' and range=='':
-        return []
+def coincidence(list='', range=''):
+    list_new = []
+    if list == '' and range == '':
+        return list_new
     for i in list:
         for j in range:
-            if type(i)==float:
-                if j<i<i+1:
-                    list_1.append(i)
-                    break
-            elif type(i)==int:
-                if i==j:
-                    list_1.append(i)
-                    break
-    return list_1
+            if type(i) == int:
+                if i == j:
+                    list_new.append(i)
+            if type(i) == float:
+                if j<i<j+1:
+                    list_new.append(i)
+    return list_new
 
-list_1=[]
-coincidence([1, 2, 3, 4, 5], range(3, 6))    #--> [3,4,5]
-coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4))   #-->[1,2,2.5]
-coincidence(list='',range='')   #-->[]
+coincidence([1, 2, 3, 4, 5], range(3, 6))  # --> [3, 4, 5]
+coincidence()  # --> []
+coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4))  # --> [1, 2, 2.5]
