88f431456b96237f4af6d33b84e23820e14d7abe
Denis <deniska.bayandin@yandex.ru>
2022-01-14 16:28:19 +0300

    edited_task_06

--- task_06.py
+++ task_06.py
@@ -1,43 +1,48 @@
 class WrongNumberOfPlayersError(Exception):
-    '''класс исключения для количества игроков'''
+    ''' класс исключения для количества игроков '''
 class NoSuchStrategyError(Exception):
-    '''класс исключения для не правильно заданых данных хода'''
+    ''' класс исключения для не правильно заданых данных хода '''
 class ExpectedTwoValuesError(Exception):
-    '''класс искючения если подаётся более двух аргументов в внутреннем списке '''
+    ''' класс искючения если подаётся более двух аргументов в внутреннем списке '''
 def rps_game_winner(players: list):
-    if len(players) >= 3:
-        raise WrongNumberOfPlayersError
-    else:
-        st = "'"
-        new_list = []
-        for i in players:
-            if len(i) >= 3:
-                raise ExpectedTwoValuesError
-        for j in players:
-            new_list+=j
-            if j[1] == 'P' or j[1] == 'S'or j[1] == 'R':
-                continue
-            else:
-                raise NoSuchStrategyError
-        if new_list[1] == new_list[3]:
-            return st + 'player1 %s' % new_list[1] + st
+    try:
+        if len(players) >= 3:
+            raise WrongNumberOfPlayersError
         else:
-            if new_list[1] == 'P' and new_list[3] == 'R' or new_list[1] == 'S' and new_list[3] == 'P' or new_list == 'R' and new_list == 'S':
-                return st + 'player1 %s' % new_list[1] + st
+            new_list = []
+            for i in players:
+                if len(i) >= 3:
+                    raise ExpectedTwoValuesError
+            for j in players:
+                new_list += j
+                if j[1] == 'P' or j[1] == 'S' or j[1] == 'R':
+                    continue
+                else:
+                    raise NoSuchStrategyError
+            if new_list[1] == new_list[3]:
+                return 'player1 %s' % new_list[1]
             else:
-                return st + 'player2 %s' % new_list[3] + st
+                if new_list[1] == 'P' and new_list[3] == 'R' or new_list[1] == 'S' and new_list[
+                    3] == 'P' or new_list == 'R' and new_list == 'S':
+                    return 'player1 %s' % new_list[1]
+                else:
+                    return 'player2 %s' % new_list[3]
+    except WrongNumberOfPlayersError:
+        print('Number of players > 2')
+    except NoSuchStrategyError:
+        print('Player moves are different R, P, S ')
+    except ExpectedTwoValuesError:
+        print('more than two arguments of the players moves are given')
 
-players_1=[['player1', 'P'], ['player2', 'P'], ['player3', 'S']]
-rps_game_winner(players_1)  #--> WrongNumberOfPlayersError
-players_1=[['player1', 'P'], ['player2', 'A']]
-rps_game_winner(players_1)  #--> NoSuchStrategyError
-players_1=[['player1', 'P'], ['player2', 'S ']]
-rps_game_winner(players_1)  #--> 'player2 S'
-players_1=[['player1', 'P'], ['player2', 'P']]
-rps_game_winner(players_1)  #--> 'player1 P'
-
-
-
-
+#players_1 = [['player1', 'P'], ['player2', 'P'], ['player3', 'S']]
+#rps_game_winner(players_1)  # --> WrongNumberOfPlayersError
+#players_1 = [['player1', 'P'], ['player2', 'A']]
+#rps_game_winner(players_1)  # --> NoSuchStrategyError
+#players_1 = [['player1', 'P'], ['player2', 'S']]
+#rps_game_winner(players_1)  # --> player2 S
+#players_1 = [['player1', 'P'], ['player2', 'P']]
+#rps_game_winner(players_1) # --> player1 P
+players_1 = [['player1','P','S'], ['player2','P']]
+rps_game_winner(players_1)  # --> ExpectedTwoValuesError
 
 
